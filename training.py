1d3bc276072b793b4fd1dfde203ab94197e3f71e
Phu Hoang <152080248+PhuHoangTruong0208@users.noreply.github.com>
2024-05-04 08:05:01 +0700

    Update training.py

--- training.py
+++ training.py
@@ -76,7 +76,7 @@ class TextDataset(Dataset):
     
 
 class TransformersTraining:
-    def __init__(self, d_model=512, batch_size=30, ffn_hidden=2048, num_heads=8, dropout=0.1, vocab_size=len(GetIndex().vocab)+1,
+    def __init__(self, d_model=512, batch_size=30, ffn_hidden=2048, num_heads=8, dropout=0.1, vocab_size=len(GetIndex().vocab),
                  num_layers=2, epochs=40):
         self.d_model = d_model
         self.batch_size = batch_size
@@ -92,7 +92,7 @@ class TransformersTraining:
         # setup model
         self.index_to_char, self.char_to_index = self.get_index.get_index()
         self.transformer_model = TranformersModel(d_model=d_model, ffn_hidden=ffn_hidden, num_heads=num_heads,
-                                    dropout=dropout, num_layers=num_layers, to_index=self.char_to_index, max_sequence_length=self.preprocess.sequence_length,
+                                    dropout=dropout, num_layers=num_layers, to_index=self.char_to_index, max_sequence_length=self.preprocess.sequence_length, vocab_size=vocab_size,
                                     START_TOKEN=self.get_index.START_TOKEN, END_TOKEN=self.get_index.END_TOKEN, PADDING_TOKEN=self.get_index.PADDING_TOKEN)
         for params in self.transformer_model.parameters():
             if params.dim() > 1:
@@ -108,21 +108,21 @@ class TransformersTraining:
         self.transformer_model.to(device)
 
         dataset = TextDataset()
-        train_loader = DataLoader(dataset, self.batch_size)
-        self.iterator = iter(train_loader)
+        self.train_loader = DataLoader(dataset, self.batch_size)
 
     def training(self):
         index_to_char, char_to_index = self.get_index.get_index()
         for epoch in range(self.epochs):
             print(f"Epoch : {epoch}")
-            for batch_num, batch in enumerate(self.iterator):
+            iterator = iter(self.train_loader)
+            for batch_num, batch in enumerate(iterator):
                 self.transformer_model.train()
                 source_batch, target_batch = batch
                 encoder_self_attention_mask, decoder_self_attention_mask, decoder_cross_attention_mask = self.create_mask(source_batch, target_batch)
                 self.optim.zero_grad()
                 target_predict = self.transformer_model(source_batch, target_batch, encoder_self_attention_mask.to(device),
                                                         decoder_self_attention_mask.to(device), decoder_cross_attention_mask.to(device),
-                                        enc_start_token=False, enc_end_token=False, dec_start_token=True, dec_end_token=True)
+                                                        enc_start_token=False, enc_end_token=False, dec_start_token=True, dec_end_token=True)
                 labels = self.transformer_model.decoder_layer.embedding.batch_tokenize(batch=target_batch, start_token=False, end_token=True)
                 loss = self.criterian(
                     target_predict.view(-1, self.vocab_size).to(device),
@@ -132,10 +132,27 @@ class TransformersTraining:
                 loss = loss.sum() / valid_indicies.sum()
                 loss.backward()
                 self.optim.step()
+                
                 if batch_num % 100 == 0:
                     print(f"Iteration {batch_num} : {loss.item()}")
-        print("đã hoàn thành huấn luyện")
+                    print("X: hello what is your name?")
+                    print("Predict: ", end="", flush=True)
 
+                    source_senttence = ("hello what is your name?",)
+                    target_sentence = ("", )
+                    for word_counter in range(int(self.preprocess.sequence_length/1.5)):
+                        encoder_self_attention_mask, decoder_self_attention_mask, decoder_cross_attention_mask = self.create_mask(source_senttence, target_sentence)
+                        target_predict_test = self.transformer_model(source_senttence, target_sentence, encoder_self_attention_mask.to(device),
+                                                        decoder_self_attention_mask.to(device), decoder_cross_attention_mask.to(device),
+                                                        enc_start_token=False, enc_end_token=False, dec_start_token=True, dec_end_token=True)
+                        next_token_prob_distribution = target_predict_test[0][word_counter]
+                        next_token_index = torch.argmax(next_token_prob_distribution).item()
+                        next_token = self.index_to_char[next_token_index]
+                        target_sentence = (target_sentence[0] + next_token, )
+                        if next_token == self.get_index.END_TOKEN:
+                            break
+                        print(next_token, end="", flush=True)
+                    print()
 
     def predict(self, source_sequence):
         index_to_char, char_to_index = self.get_index.get_index()
